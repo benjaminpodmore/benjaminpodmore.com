99c7ca8219113bef7770dafa9a5e76727c6cf863
Benjamin Podmore <benjaminpodmore@gmail.com>
2023-09-13 15:27:34 +0800

    make buttons more modern

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,9 @@ function App() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="transform -translate-y-16">
-        <div className="mb-4 text-center">
-          <button onClick={() => setView('about')} className="border rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 mr-2 w-24">About</button>
-          <button onClick={() => setView('projects')} className="border rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 w-24">Projects</button>
+        <div className="mb-4 flex items-center justify-center">
+          <button onClick={() => setView('about')} className="border rounded-full px-4 py-2 hover:bg-gray-100 w-24 mr-1">About</button>
+          <button onClick={() => setView('projects')} className="border rounded-full px-4 py-2 hover:bg-gray-100 w-24 ml-1">Projects</button>
         </div>
         <div className="border-t border-b border-gray-200 py-6 max-w-md m-auto h-56 flex flex-col items-center justify-center">
           <AnimatePresence mode="wait">
