d03ecfbf86064a96c9802a308102771851e724c9
Benjamin Podmore <benjaminpodmore@gmail.com>
2023-10-10 11:53:08 +0800

    Fix typo in nlp project

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -4,7 +4,7 @@ const Projects = () => {
       <h2 className="text-2xl font-semibold mb-4">Projects</h2>
       <div className="mb-6">
         <h3 className="text-xl font-medium mb-1">2021: NLP Honours</h3>
-        <p>I did my honours in NLP. It revolved around coreference resolution and being able to identify when an atecedent is too vague to resolve.</p>
+        <p>I did my honours in NLP. It revolved around coreference resolution and being able to identify when an antecedent is too vague to resolve.</p>
       </div>
     </section>
   );
