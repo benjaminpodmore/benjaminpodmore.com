2c05c70a3d3a30f0312f8df43b98bcf8c25eac67
Benjamin Podmore <benjaminpodmore@gmail.com>
2023-10-14 15:00:54 +0800

    fix view for mobile

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -8,8 +8,8 @@ const Landing = () => {
     });
 
     return (
-        <div id="landing" className="h-[80vh] flex flex-col justify-center items-center snap-start">
-            <animated.h1 style={fade} className="text-4xl font-bold mb-6">Hello, I'm Benjamin Podmore</animated.h1>
+        <div id="landing" className="h-[80vh] flex flex-col justify-center items-center snap-start pt-14">
+            <animated.h1 style={fade} className="text-4xl font-bold mb-6 text-center">Hello, I'm Benjamin Podmore</animated.h1>
             <div className="relative w-[300px] h-[300px] rounded-full overflow-hidden border-4 border-blue-600">
                 <animated.img style={fade} src="portrait-min-zoom.jpg" alt="Your Portrait" className="absolute top-0 left-0 w-full h-full object-cover"/>
             </div>
