5107acd3dd137a57b0fb77ed1e3a99af67cf010e
Benjamin Podmore <benjaminpodmore@gmail.com>
2023-10-14 15:10:12 +0800

    dynamic navbar stickiness

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <nav className="bg-black p-4 sticky top-0 z-50 h-14">
+        <nav className="bg-black p-4 sm:sticky top-0 z-50 h-14">
             <ul className="flex space-x-4">
                 <li><a href="#landing" className="text-blue-600 hover:text-blue-400">Home</a></li>
                 <li><a href="#about" className="hover:text-gray-400">About</a></li>
